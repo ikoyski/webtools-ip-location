7ef2d9299c018c063a51640a67dc80f3c79b784c
CRSanpedro <CRSanpedro@F10220.rcgitmanila.com>
2022-07-22 22:23:46 +0800

    initial commit

---
+++ src/main/java/com/ikoyski/webtoolsiplocation/WebtoolsIpLocationApplication.java
@@ -0,0 +1,13 @@
+package com.ikoyski.webtoolsiplocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoolsIpLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtoolsIpLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikoyski/webtoolsiplocation/WebtoolsIpLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikoyski.webtoolsiplocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtoolsIpLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
