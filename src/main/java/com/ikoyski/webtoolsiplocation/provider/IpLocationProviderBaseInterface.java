3ff897571368f337ef36df2547733c07656c9e03
CRSanpedro <CRSanpedro@F10220.rcgitmanila.com>
2022-07-26 13:21:37 +0800

    test

--- src/main/java/com/ikoyski/webtoolsiplocation/provider/IpLocationProviderBaseInterface.java
+++ src/main/java/com/ikoyski/webtoolsiplocation/provider/IpLocationProviderBaseInterface.java
@@ -4,6 +4,8 @@ import com.ikoyski.webtoolsiplocation.dto.IpLocationResponse;
 
 public interface IpLocationProviderBaseInterface {
 
+	public static final String ERROR_INVALID_URI = "invalid uri";
+
 	public IpLocationResponse getIpLocation(String ip);
 
 }
--- src/main/java/com/ikoyski/webtoolsiplocation/provider/IpLocationProviderIpApi.java
+++ src/main/java/com/ikoyski/webtoolsiplocation/provider/IpLocationProviderIpApi.java
@@ -1,6 +1,13 @@
 package com.ikoyski.webtoolsiplocation.provider;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ikoyski.webtoolsiplocation.dto.IpLocationResponse;
 
@@ -8,9 +15,16 @@ public class IpLocationProviderIpApi implements IpLocationProviderBaseInterface 
 
 	@Override
 	public IpLocationResponse getIpLocation(String ip) {
-		String uri = "http://ip-api.com/json/" + ip;
-		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate.getForObject(uri, IpLocationResponse.class);
+		IpLocationResponse ipLocationResponse = null;
+		try {
+			URI uri = new URI("http://ip-api.com/json/" + ip);
+			RestTemplate restTemplate = new RestTemplate();
+			ipLocationResponse = restTemplate.getForObject(uri.toURL().toString(), IpLocationResponse.class);
+		} catch (URISyntaxException | RestClientException | MalformedURLException e) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT, IpLocationProviderBaseInterface.ERROR_INVALID_URI,
+					null);
+		}
+		return ipLocationResponse;
 	}
 
 }
