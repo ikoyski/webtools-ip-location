572fec27ee98e0c23ca0a0b6470e98c92ad99d25
CRSanpedro <CRSanpedro@2023RLD0385-LT.rcgitmanila.com>
2025-02-23 10:01:44 -0800

    on localhost: service registry, config server connection ok

--- src/main/java/com/ikoyski/webtools/iplocation/provider/IpLocationProviderFactory.java
+++ src/main/java/com/ikoyski/webtools/iplocation/provider/IpLocationProviderFactory.java
@@ -1,12 +1,16 @@
 package com.ikoyski.webtools.iplocation.provider;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IpLocationProviderFactory {
 
-	public static final String PROVIDER_IPAPI = "IpApi";
+	public static final String PROVIDER_IPAPI = "IPAPI";
+
+	@Autowired
+	IpLocationProviderIpApi ipLocationProviderIpApi;
 
 	@Value("${provider.default}")
 	private String PROVIDER_DEFAULT;
@@ -17,8 +21,7 @@ public class IpLocationProviderFactory {
 
 	public IpLocationProviderBaseInterface createIpLocationProvider(String type) {
 		if (PROVIDER_IPAPI.equals(type)) {
-			// using https://ip-api.com/
-			return new IpLocationProviderIpApi();
+			return ipLocationProviderIpApi;
 		} else {
 			throw new IllegalArgumentException("No such provider.");
 		}
--- src/test/java/com/ikoyski/webtools/iplocation/IpLocationProviderIpApiTest.java
+++ src/test/java/com/ikoyski/webtools/iplocation/IpLocationProviderIpApiTest.java
@@ -3,23 +3,27 @@ package com.ikoyski.webtools.iplocation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.ikoyski.webtools.iplocation.dto.IpLocationResponse;
 import com.ikoyski.webtools.iplocation.provider.IpLocationProviderBaseInterface;
 import com.ikoyski.webtools.iplocation.provider.IpLocationProviderFactory;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IpLocationProviderIpApiTest {
+
+	@Autowired
+	IpLocationProviderFactory ipLocationProviderFactory;
 
 	@Test
 	@DisplayName("IpLocationProviderIpApiTest.ipLocationProviderIpApiSuccess()")
 	void ipLocationProviderIpApiSuccess() {
 		// given
 		final String IP = "8.8.8.8";
-		IpLocationProviderFactory ipLocationProviderFactory = new IpLocationProviderFactory();
-		IpLocationProviderBaseInterface ipLocationProvider = ipLocationProviderFactory
-				.createIpLocationProvider(IpLocationProviderFactory.PROVIDER_IPAPI);
+		IpLocationProviderBaseInterface ipLocationProvider = ipLocationProviderFactory.createIpLocationProvider();
 
 		// when
 		IpLocationResponse ipLocationResponse = ipLocationProvider.getIpLocation(IP);
@@ -33,9 +37,7 @@ class IpLocationProviderIpApiTest {
 	void ipLocationProviderIpApiException() {
 		// given
 		final String IP = "\\";
-		IpLocationProviderFactory ipLocationProviderFactory = new IpLocationProviderFactory();
-		IpLocationProviderBaseInterface ipLocationProvider = ipLocationProviderFactory
-				.createIpLocationProvider(IpLocationProviderFactory.PROVIDER_IPAPI);
+		IpLocationProviderBaseInterface ipLocationProvider = ipLocationProviderFactory.createIpLocationProvider();
 
 		// when and then
 		Assertions.assertThrows(Exception.class, () -> ipLocationProvider.getIpLocation(IP));
