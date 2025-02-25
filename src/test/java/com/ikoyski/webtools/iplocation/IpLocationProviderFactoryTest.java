1b5e18c6dacdf75df8352cabec35bbc0c72bb43f
ikoyski <christian_sanpedro@yahoo.com>
2025-02-25 19:51:14 +0000

    added different profiles for different deployments

--- src/test/java/com/ikoyski/webtools/iplocation/IpLocationProviderFactoryTest.java
+++ src/test/java/com/ikoyski/webtools/iplocation/IpLocationProviderFactoryTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.ikoyski.webtools.iplocation.provider.IpLocationProviderBaseInterface;
 import com.ikoyski.webtools.iplocation.provider.IpLocationProviderFactory;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IpLocationProviderFactoryTest {
 
 	@Autowired
