3a730fb75dde8fe2d34e573f3521167090648f4d
ikoyski <christian_sanpedro@yahoo.com>
2025-02-28 17:31:29 +0000

    updated request mappings

--- src/main/java/com/ikoyski/webtools/iplocation/controller/IpLocationController.java
+++ src/main/java/com/ikoyski/webtools/iplocation/controller/IpLocationController.java
@@ -3,12 +3,14 @@ package com.ikoyski.webtools.iplocation.controller;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ikoyski.webtools.iplocation.dto.IpLocationResponse;
 import com.ikoyski.webtools.iplocation.service.IpLocationService;
 
 @RestController
+@RequestMapping("/ip-location")
 public class IpLocationController {
 
 	private final IpLocationService ipLocationService;
@@ -17,8 +19,8 @@ public class IpLocationController {
 		this.ipLocationService = ipLocationService;
 	}
 
-	@GetMapping(path = "api/v1/ipLocation/{ip}")
-	@Cacheable(value = "ipLocation", key = "#ip")
+	@GetMapping(path = "/v1/{ip}")
+	@Cacheable(value = "ip-location", key = "#ip")
 	public IpLocationResponse getIpLocation(@PathVariable("ip") String ip) {
 		return ipLocationService.getIpLocation(ip);
 	}
